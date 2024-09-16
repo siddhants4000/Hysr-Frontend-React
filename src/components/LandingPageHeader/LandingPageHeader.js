606161e4fcbba664010f02423d40419d5823710f
Siddhant Sharma <64482329+siddhants4000@users.noreply.github.com>
2024-09-17 04:39:19 +0530

    Update LandingPageHeader.js

--- src/components/LandingPageHeader/LandingPageHeader.js
+++ src/components/LandingPageHeader/LandingPageHeader.js
@@ -11,15 +11,15 @@ const LandingPageHeader = () => {
         className="logo"
       />
       <nav className="nav">
-        <a href="#" className="nav-link">Articles</a>
-        <a href="#" className="nav-link">People</a>
-        <a href="#" className="nav-link">Learning</a>
-        <a href="#" className="nav-link">Jobs</a>
-        <a href="#" className="nav-link">Games</a>
-        <a href="#" className="nav-link">Get the app</a>
+        <a href="https://www.google.com/" className="nav-link">Articles</a>
+        <a href="https://www.google.com/" className="nav-link">People</a>
+        <a href="https://www.google.com/" className="nav-link">Learning</a>
+        <a href="https://www.google.com/" className="nav-link">Jobs</a>
+        <a href="https://www.google.com/" className="nav-link">Games</a>
+        <a href="https://www.google.com/" className="nav-link">Get the app</a>
       </nav>
       <div className="auth-buttons">
-        <a href="#" className="join-now">Join now</a>
+        <a href="https://www.google.com/" className="join-now">Join now</a>
         <a href="/login" className="sign-in">Sign in</a>
       </div>
     </header>
